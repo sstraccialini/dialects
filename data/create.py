616abf5be0c365e2e05d577230380cfa8945db12
Raffaele Simbari <raffaele.simbari23@gmail.com>
2026-04-19 19:27:22 +0200

    Add additional dataset URLs for language modeling in create.py

--- data/create.py
+++ data/create.py
@@ -12,6 +12,16 @@ dumps = [
     ('https://dumps.wikimedia.org/other/mediawiki_content_current/vecwiki/2026-04-01/xml/bzip2/vecwiki-2026-04-01-p3p154496.xml.bz2', 'vec_texts'),
     ('https://dumps.wikimedia.org/other/mediawiki_content_current/lmowiki/2026-04-01/xml/bzip2/lmowiki-2026-04-01-p1p279182.xml.bz2', 'lmo_texts'),
     ('https://dumps.wikimedia.org/other/mediawiki_content_current/scnwiki/2026-04-01/xml/bzip2/scnwiki-2026-04-01-p1p67086.xml.bz2', 'scn_texts'),
+    ('https://dumps.wikimedia.org/other/mediawiki_content_current/scwiki/2026-04-01/xml/bzip2/scwiki-2026-04-01-p2p23475.xml.bz2', 'sc_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/itwiki/2026-04-01/xml/bzip2/itwiki-2026-04-01-p8504027p10924272.xml.bz2,', 'it_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/eswiki/2026-04-01/xml/bzip2/eswiki-2026-04-01-p9859121p11472026.xml.bz2', 'es_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/frwiki/2026-04-01/xml/bzip2/frwiki-2026-04-01-p8575096p13577595.xml.bz2', 'fr_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/cawiki/2026-04-01/xml/bzip2/cawiki-2026-04-01-p1p2219762.xml.bz2', 'ca_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/dewiki/2026-04-01/xml/bzip2/dewiki-2026-04-01-p7853411p11412479.xml.bz2', 'de_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/elwiki/2026-04-01/xml/bzip2/elwiki-2026-04-01-p1p953362.xml.bz2)', 'el_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/arwiki/2026-04-01/xml/bzip2/arwiki-2026-04-01-p3p3707707.xml.bz2', 'ar_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/slwiki/2026-04-01/xml/bzip2/slwiki-2026-04-01-p1p600301.xml.bz2', 'sl_texts'),
+    #('https://dumps.wikimedia.org/other/mediawiki_content_current/enwiki/2026-04-01/xml/bzip2/enwiki-2026-04-01-p8168729p13235917.xml.bz2', 'en_texts'),
 ]
 
 SCRIPT_DIR = Path(__file__).resolve().parent
